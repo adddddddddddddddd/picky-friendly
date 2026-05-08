06126853702c8174bf64914e4a469037fcc82da7
adddddddddddddddd <171546698+adddddddddddddddd@users.noreply.github.com>
2026-05-08 11:02:50 +0200

    feat: add additional remote image pattern for Brave search engine

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: 'images.pexels.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'imgs.search.brave.com',
+        pathname: '/**',
+      },
     ],
   },
 }
